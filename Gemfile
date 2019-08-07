30b296510be3e8564caf9fb2bd75d0afe8b70376
Louis UGUEN <louisuguen1@gmail.com>
2019-08-07 15:09:28 +0200

    Ex done

--- Gemfile
+++ Gemfile
@@ -32,7 +32,10 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
+gem 'simple_form'
+gem 'webpacker', '~> 4.x'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+french = Restaurant.create!(name: "Epicure", category: "french", phone_number: "06 76 54 34 76", address: "Paris")
+china = Restaurant.create!(name: "Paul", category: "chinese", phone_number: "06 77 54 00 54", address: "Pekin")
+belge = Restaurant.create!(name: "Coluche", category: "belgian", phone_number: "07 34 76 00 70", address: "Bruxelles")
+italia = Restaurant.create!(name: "Pizza", category: "italian", phone_number: "06 99 44 34 76", address: "Rome")
+bretagne = Restaurant.create!(name: "Creperie", category: "french", phone_number: "06 33 54 00 44", address: "Brest")
